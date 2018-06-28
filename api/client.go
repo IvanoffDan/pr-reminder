1e12b519a685a53368718fc88d18dd4622a74c91
Daniil Ivanov <ivanoff.daniil@gmail.com>
2018-06-28 22:27:39 +1000

    Fixed permissions

--- api/client.go
+++ api/client.go
@@ -1,10 +1,1 @@
-package client
 
-// Client is an interface to connect to repository
-type Client interface {
-}
-
-// // NewClient return a new Client
-// func NewClient() (Client, error) {
-
-// }
